1b28b1671c9393e803e6d4ba05cfd8689a02ed1e
Krasimir Petranov <83006302+Krasimir0@users.noreply.github.com>
2026-02-10 18:38:30 +0000

    Fix format on smartphone mode

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+const burger = document.querySelector(".burger");
+const mobileNav = document.querySelector(".mobile-nav");
+
+burger.addEventListener("click", () => {
+  burger.classList.toggle("active");
+  mobileNav.classList.toggle("active");
+});
 
 const header = document.querySelector(".site-header");
 
